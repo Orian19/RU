b3293d5364714e32cb5bf9d95bfa5c01ccd8f813
Orian19 <orian19@gmail.com>
2023-05-11 13:17:24 +0300

    [1][B][dataStructures][5] fix merge sort + merge + started working on radixSort (does not work). note that main is currently commented out

--- FirstYear/SemesterB/dataStructures/assignment5/src/main/java/Main.java
+++ FirstYear/SemesterB/dataStructures/assignment5/src/main/java/Main.java
@@ -1,75 +1,75 @@
-import java.lang.reflect.Array;
-import java.util.Collections;
-import java.util.Random;
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Starting...");
-
-        int[] inputs = {10000, 50000, 100000, 500000, 1000000};
-        int[] thresholds = {};
-
-        Sort[] sortAlgo = {new Sort()};
-
-        Sort s = new Sort<Integer>();
-
-        int NUMITER = 100;
-        double[][] durationList = new double[6][NUMITER];
-
-        for (int n = 0; n < inputs.length; n++) {  // input size
-            for (int k = 0; k < NUMITER; k++) { // iterations for each algo
-                int [] rndArrA = randomInputs(inputs[k]);
-                int [] sortedIncArrA = Arrays.sort(rndArrA);
-                int [] sortedDecArrA = Arrays.sort(rndArrA, Collections.reverseOrder());
-            }
-            for (int i = 0; i < 6; i++) {
-                int [] rndArrB = int[rndArrA.length];
-                int [] sortedIncArrB = int[sortedIncArrA.length];
-                int [] sortedDecArrB = int[sortedDecArrA.length];
-
-                System.arraycopy(rndArrA, 0, rndArrB, 0, rndArrB.length);
-                System.arraycopy(sortedIncArrA, 0, sortedIncArrB, 0, rndArrB.length);
-                System.arraycopy(sortedDecArrA, 0, sortedDecArrB, 0, rndArrB.length);
-
-                long startTime = System.currentTimeMillis();
-                s.setNaiveSortThreshold(thresholds[i]);
-                sortAlgo[i].(rndArrB);
-                sortAlgo[i].(sortedIncArrB);
-                sortAlgo[i].(sortedDecArrB);
-                long endTime = System.currentTimeMillis();
-                durationList[i][k]= ((double) (endTime - startTime));
-            }
-        }
-
-        double[] sum = new double[6];
-        double[] std = new double[6];
-        for (int i = 0; i < durationList.length; i++) {
-            for (int j = 0; j < durationList[0].length; j++) {
-                double curValue = durationList[i][j];
-                sum[i] += curValue;
-                std[i] += Math.pow(curValue - sum)
-            }
-            double avg = sum[i]/NUMITER;
-
-            System.out.println("The average running time for - " + durationList[i] + "is: " + avg);
-            System.out.println("The standard deviation for - " + durationList[i] + "is: " + std[i]);
-        }
-    }
-
-    //TODO: fix all functions the answer need of range [0, Integer.MAX_VALUE]
-
-    /**
-     * random array of integers in range [0, Integer.MAX_VALUE]
-     * @param size of input
-     * @return randomized array
-     */
-    public static  int[] randomInputs(int size) {
-        int [] nums = new int [size];
-        Random randomGenerator = new Random();
-        for(int i = 0 ; i < nums.length; i++){
-            nums[i] = randomGenerator.nextInt(Integer.MAX_VALUE);
-        }
-        return nums;
-    }
-}
+//import java.lang.reflect.Array;
+//import java.util.Collections;
+//import java.util.Random;
+//import java.util.Arrays;
+//
+//public class Main {
+//    public static void main(String[] args){
+//        System.out.println("Starting...");
+//
+//        int[] inputs = {10000, 50000, 100000, 500000, 1000000};
+//        int[] thresholds = {};
+//
+//        Sort[] sortAlgo = {new Sort()};
+//
+//        Sort s = new Sort<Integer>();
+//
+//        int NUMITER = 100;
+//        double[][] durationList = new double[6][NUMITER];
+//
+//        for (int n = 0; n < inputs.length; n++) {  // input size
+//            for (int k = 0; k < NUMITER; k++) { // iterations for each algo
+//                int [] rndArrA = randomInputs(inputs[k]);
+//                int [] sortedIncArrA = Arrays.sort(rndArrA);
+//                int [] sortedDecArrA = Arrays.sort(rndArrA, Collections.reverseOrder());
+//            }
+//            for (int i = 0; i < 6; i++) {
+//                int [] rndArrB = int[rndArrA.length];
+//                int [] sortedIncArrB = int[sortedIncArrA.length];
+//                int [] sortedDecArrB = int[sortedDecArrA.length];
+//
+//                System.arraycopy(rndArrA, 0, rndArrB, 0, rndArrB.length);
+//                System.arraycopy(sortedIncArrA, 0, sortedIncArrB, 0, rndArrB.length);
+//                System.arraycopy(sortedDecArrA, 0, sortedDecArrB, 0, rndArrB.length);
+//
+//                long startTime = System.currentTimeMillis();
+//                s.setNaiveSortThreshold(thresholds[i]);
+//                sortAlgo[i].(rndArrB);
+//                sortAlgo[i].(sortedIncArrB);
+//                sortAlgo[i].(sortedDecArrB);
+//                long endTime = System.currentTimeMillis();
+//                durationList[i][k]= ((double) (endTime - startTime));
+//            }
+//        }
+//
+//        double[] sum = new double[6];
+//        double[] std = new double[6];
+//        for (int i = 0; i < durationList.length; i++) {
+//            for (int j = 0; j < durationList[0].length; j++) {
+//                double curValue = durationList[i][j];
+//                sum[i] += curValue;
+//                std[i] += Math.pow(curValue - sum)
+//            }
+//            double avg = sum[i]/NUMITER;
+//
+//            System.out.println("The average running time for - " + durationList[i] + "is: " + avg);
+//            System.out.println("The standard deviation for - " + durationList[i] + "is: " + std[i]);
+//        }
+//    }
+//
+//    //TODO: fix all functions the answer need of range [0, Integer.MAX_VALUE]
+//
+//    /**
+//     * random array of integers in range [0, Integer.MAX_VALUE]
+//     * @param size of input
+//     * @return randomized array
+//     */
+//    public static  int[] randomInputs(int size) {
+//        int [] nums = new int [size];
+//        Random randomGenerator = new Random();
+//        for(int i = 0 ; i < nums.length; i++){
+//            nums[i] = randomGenerator.nextInt(Integer.MAX_VALUE);
+//        }
+//        return nums;
+//    }
+//}
--- FirstYear/SemesterB/dataStructures/assignment5/src/main/java/Sort.java
+++ FirstYear/SemesterB/dataStructures/assignment5/src/main/java/Sort.java
@@ -2,10 +2,9 @@ import java.lang.reflect.Array;
 
 public class Sort <T extends Comparable<T>> {
     private int threshold;
-    private T[] array; // TODO: figure out if needed other fields
 
     public Sort() {
-        this.threshold = 50;//mergeRecursive = 50000; quickClass = 50;
+        this.threshold = 0;//mergeRecursive = 50000; quickClass = 50;
     }
 
     /**
@@ -109,16 +108,6 @@ public class Sort <T extends Comparable<T>> {
     }
 
     /**
-     * This is the standard, unmodified version of the radix sort which gets as an input the base to be
-     * used by the algorithm.
-     * @param array to sort
-     * @param base to be used
-     */
-    public static void radixSort(int[] array, int base){
-
-    }
-
-    /**
      * This function sorts an array using recursive stable merge sort.
      * @param array to sort
      */
@@ -150,51 +139,65 @@ public class Sort <T extends Comparable<T>> {
      * @param mid pointer
      * @param end pointer
      */
-    private void merge(T[] array, int start, int mid, int end) { // TODO: something wrong. I get duplicate values
-        int n = end - start + 1;
-        T[] tempArr = (T[]) Array.newInstance(Comparable.class, n);
-        int i = 0;
-        int i1 = start, i2 = mid + 1;
-
-        while (i < n) {
-            if(array[i1].compareTo(array[i2]) < 0) { // checking if A[i1] < A[i2]
-                tempArr[i++] = array[i1++];
-            } else {
-                tempArr[i++] = array[i2++];
-            }
-        }
-
-        // copying the merged array into the input array
-        for (int j = 0; j < n-1; j++) {
-            array[start+j] = tempArr[j]; // TODO: maybe issue is this is too slow and not memory efficient?
-        }
-//        System.arraycopy(array, start, tempArr, start, n-start); // copying elements to the new array
-    }
-
 //    private void merge(T[] array, int start, int mid, int end) { // TODO: something wrong. I get duplicate values
-//        int n1 = mid - start + 1, n2 = end - mid;
-//        T[] left = (T[]) Array.newInstance(Comparable.class, n1);
-//        T[] right = (T[]) Array.newInstance(Comparable.class, n2);
+//        int n = end - start + 1;
+//        T[] tempArr = (T[]) Array.newInstance(Comparable.class, n);
+//        int i = 0;
+//        int i1 = start, i2 = mid + 1;
 //
-//        for (int i = 0; i < n1; i++) {
-//            left[i] = array[start+i];
-//        }
-//
-//        for (int i = 0; i < n2; i++) {
-//            right[i] = array[mid+1+i];
-//        }
-//
-//        int i = 0, j = 0;
-//        for (int k = start; k < end; k++) {
-//            if (left[i].compareTo(right[j]) < 0) { // checking if L[i] < R[J]
-//                array[k] = left[i];
-//                i++;
+//        while (i < n) {
+//            if(array[i1].compareTo(array[i2]) < 0) { // checking if A[i1] < A[i2]
+//                tempArr[i++] = array[i1++];
 //            } else {
-//                array[k] = right[j];
-//                j++;
+//                tempArr[i++] = array[i2++];
 //            }
 //        }
+//
+//        // copying the merged array into the input array
+//        for (int j = 0; j < n-1; j++) {
+//            array[start+j] = tempArr[j]; // TODO: maybe issue is this is too slow and not memory efficient?
+//        }
+////        System.arraycopy(array, start, tempArr, start, n-start); // copying elements to the new array
 //    }
+
+    private void merge(T[] array, int start, int mid, int end) { // TODO: something wrong. I get duplicate values
+        int n1 = mid - start + 1, n2 = end - mid;
+        T[] left = (T[]) Array.newInstance(Comparable.class, n1);
+        T[] right = (T[]) Array.newInstance(Comparable.class, n2);
+
+        for (int i = 0; i < n1; i++) {
+            left[i] = array[start+i];
+        }
+
+        // TODO: copy helper function
+        for (int i = 0; i < n2; i++) {
+            right[i] = array[mid+1+i];
+        }
+
+        int i = 0, j = 0, k = start;
+        while (i < left.length && j < right.length) {
+            if (left[i].compareTo(right[j]) <= 0) { // checking if L[i] < R[J]
+                array[k] = left[i];
+                i++;
+            } else {
+                array[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        // copying left overs to final array
+        while (i < left.length) {
+            array[k] = left[i];
+            k++;
+            i++;
+        }
+        while (j < right.length) {
+            array[k] = right[j];
+            k++;
+            j++;
+        }
+    }
 
     /**
      * helper function that implemnt bubble sort.
@@ -251,6 +254,119 @@ public class Sort <T extends Comparable<T>> {
         }
     }
 
+    /**
+     * This is the standard, unmodified version of the radix sort which gets as an input the base to be
+     * used by the algorithm. uses count sort
+     * @param array to sort
+     * @param base to be used
+     */
+    //TODO: fixxxxx, does not work + I dont fully understand some parts
+    public static void radixSort(int[] array, int base) {
+        int maxNumDigits = (int) Math.log(base); //TODO: probably issue here! // calculating the max num of digits for a specific base
+        // TODO: do we also need to deal with letters? for ex. hexadecimal
+        convertBase(array, 10, base);
+        int sizeDigits = getMaxSize(array, base);
+
+        for (int i = 0; i < sizeDigits; i += maxNumDigits) {
+            int[] sortedBySubNum =  new int[array.length]; // starting from LSB to MSB
+            countSort(array, sortedBySubNum, sizeDigits, i, base);
+            System.arraycopy(sortedBySubNum, 0, array, 0, array.length);
+        }
+        convertBase(array, base, 10);
+    }
+
+    /**
+     * finding the bound for radix sort
+     * @param array given
+     * @param base given
+     * @return max size of digits
+     */
+    private static int getMaxSize(int[] array, int base) {
+        // TODO: not sure how this works or if needed
+        int maxElement = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxElement) {
+                maxElement = array[i];
+            }
+        }
+        return  (int) Math.ceil(Math.log(maxElement + 1) / Math.log(base));
+    }
+
+    /**
+     * helper function that implements countSort for a specific base
+     * @param array to sort
+     * @param digitIdx amount of digits representing one number in the given base
+     * @param base working with (all elements in the array are from this base)
+     * @param sorted result of the sorted array will be here
+     * @param sizeBound the maximum number of digits in the given array (our k)
+     */
+    private static void countSort(int [] array, int [] sorted, int sizeBound, int digitIdx, int base) {
+        // TODO: need to somehow get the max num of digits
+        // TODO: need somehow integrate digitIdx and base!!!
+        // initializing the prefix sum array (C in the lecture)
+        int[] prefixSum = new int[base]; //TODO: not sure about the size (sizeBoound?)
+
+        // building C to hold the number of appearance of each element s.t it corresponds to index of C
+        for (int i = 0; i < array.length; i++) { // TODO: should start from 0/1?
+            int curDigit= getCurDigit(array[i], digitIdx, base);
+            prefixSum[curDigit]++;
+        }
+
+        // now C[i] contains the number of elements equal to i
+        // we now create C prime (modifying C to have prefix sum)
+        for (int i = 1; i < base; i++) {
+            prefixSum[i] += prefixSum[i-1];
+        }
+
+        // now C prime = prefix sum contains the number of elements less than or equal to i
+        // we now fill the sorted(B) with the correct values by prefixSum to get the sorted array
+        for (int i = array.length-1; i >= 0; i--) { //TODO: make sure for is good
+            int curDigit = getCurDigit(array[i], digitIdx, base);
+            sorted[prefixSum[curDigit] - 1] = array[i]; //TODO: should there be -1 here?
+            prefixSum[curDigit]--;
+        }
+    }
+
+    private static int getCurDigit(int curValue, int digIdx, int base) {
+        // TODO: need to convert. is this what I need?
+        return (curValue / (int)Math.pow(base, digIdx)) % base;
+    }
+
+    /**
+     * converting a number from one base to another
+     * @param array given
+     * @param fromBase from which base to convert
+     * @param toBase to which base to convert
+     */
+    private static void convertBase(int[] array, int fromBase, int toBase) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = convertNumber(array[i], fromBase, toBase);
+        }
+    }
+
+    private static int convertNumber(int number, int fromBase, int toBase) {
+        return convertFromDecimal(convertToDecimal(number, fromBase), toBase);
+    }
+
+    private static int convertToDecimal(int number, int base) {
+        int decimal = 0;
+        int power = 0;
+        while (number > 0) {
+            decimal += (number % 10) * Math.pow(base, power++);
+            number /= 10;
+        }
+        return decimal;
+    }
+
+    private static int convertFromDecimal(int decimal, int base) {
+        int number = 0;
+        int power = 0;
+        while (decimal > 0) {
+            number += (decimal % base) * (int) Math.pow(10, power++);
+            decimal /= base;
+        }
+        return number;
+    }
 
     /**
      * This function updates the value of a private data member that controls the size of inputs below
