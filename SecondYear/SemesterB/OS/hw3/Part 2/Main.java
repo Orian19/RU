fdebed44b4e2e7e9def73a494f7501977a9ccb7c
Orian19 <orian19@gmail.com>
2024-05-16 18:25:19 +0300

    [2][B][OS][3] part2 implement Main

--- SecondYear/SemesterB/OS/hw3/Part 2/Main.java
+++ SecondYear/SemesterB/OS/hw3/Part 2/Main.java
@@ -1,4 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -27,7 +35,36 @@ public class Main {
         //Assuming X is the number of cores - Partition the data into x data sets
         //Create a fixed thread pool of size X
         //Submit x workers to the thread pool
-		//Wait for termination
+        //Wait for termination
+        int coresNum = Runtime.getRuntime().availableProcessors(); // get number of available cores
+        List<List<String>> dataSets = new ArrayList<>();
+
+        // partition the data int coresNum data sets (using subList)
+        int partitionsNum = lines.size() / coresNum;
+        int startIdx = 0, endIdx;
+        for (int i = 0; i < coresNum; i++) {
+            endIdx = startIdx + partitionsNum;
+            if (i == coresNum - 1)
+                endIdx = lines.size();
+            dataSets.add(lines.subList(startIdx, endIdx));
+            startIdx = endIdx;
+        }
+
+        // creating a fixed thread pool of size coresNum (achieves maximum efficiency) and submit workers to the pool
+        ExecutorService pool = Executors.newFixedThreadPool(coresNum);
+        for (List<String> dataSet : dataSets) {
+            pool.submit(new Worker(dataSet));
+        }
+
+        // wait for termination
+        pool.shutdown();
+        try {
+            if (!pool.awaitTermination(10, TimeUnit.SECONDS)) {
+                pool.shutdown();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void workWithoutThreads(List<String> lines) {
@@ -39,6 +76,19 @@ public class Main {
         //Your code:
         //Read the shakespeare file provided from C:\Temp\Shakespeare.txt
         //and return an ArrayList<String> that contains each line read from the file.
-        return null;
+
+        String filePath = "C:\\Temp\\shakespear.txt";
+        List<String> lines = new ArrayList<>();
+        String line;
+        // reading line by line an adding to the list
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return lines;
     }
+
 }
