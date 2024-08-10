24ede9781b61572c5ac51706cc8ea5b993f8db91
Orian19 <orian19@gmail.com>
2024-08-10 12:42:50 +0300

    [3][][C#][1] final version ex01

---
+++ SecondYear/SummerSemester/CSharp/C24 Ex01 TomerCohen 314831389 OrianDaniel 208126094/Ex01_03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex01_03
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(string.Format("The height is: {0}", 3));
+            Ex01_02.Program.PrintABCTreeRecursive(3);
+            Console.WriteLine(string.Format("The height is: {0}", 5));
+            Ex01_02.Program.PrintABCTreeRecursive(5);
+            Console.WriteLine(string.Format("The height is: {0}", 6));
+            Ex01_02.Program.PrintABCTreeRecursive(6);
+            Console.WriteLine(string.Format("The height is: {0}", 9));
+            Ex01_02.Program.PrintABCTreeRecursive(9);
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
