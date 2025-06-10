5f1a512418086bf36e051331d1c9d3e503d9c739
Orian19 <orian19@gmail.com>
2025-06-10 19:02:07 +0300

    [3][B][FunctionalProgramming][5] implement foldr and foldOccur + add Foldable import + add implementations from HW4

--- ThirdYear/SemesterB/FunctionalProgramming/HW5/MultiSet.hs
+++ ThirdYear/SemesterB/FunctionalProgramming/HW5/MultiSet.hs
@@ -10,41 +10,85 @@ import Data.Map qualified as Map
 import Data.Maybe
 import Data.Semigroup (Arg (..))
 import Data.Set qualified as Set
-import Prelude (Bool (..), Char, Double, Either (..), Eq (..), Int, Integer, Integral, Maybe (..), Monoid (..), Num (..), Ord (..), Semigroup (..), Show (..), String, all, const, div, drop, error, filter, foldl', foldr, id, init, iterate, length, lookup, map, mod, not, otherwise, product, replicate, reverse, sum, undefined, zip, zipWith, (!!), ($), (&&), (++), (.), (^), (||))
+import Prelude (Bool (..), Char, Double, Either (..), Eq (..), Int, Integer, Integral, Maybe (..), Monoid (..), Num (..), Ord (..), Semigroup (..), Foldable (foldr), Show (..), String, all, const, div, drop, error, filter, foldl', foldr, id, init, iterate, length, lookup, map, mod, not, otherwise, product, replicate, reverse, sum, undefined, zip, zipWith, (!!), ($), (&&), (++), (.), (^), (||))
 
 newtype MultiSet a = MultiSet {_getMultiset :: Set.Set (Arg a Int)}
 
+lookupS :: Ord a => a -> Set.Set (Arg a b) -> Maybe b
+lookupS x s = case Set.lookupGE (Arg x undefined) s of
+  Just (Arg y b) | x == y -> Just b
+  _ -> Nothing
+
 empty :: MultiSet a
+empty = MultiSet Set.empty
 
 member :: Ord a => a -> MultiSet a -> Bool
+member x m = case lookupS x (_getMultiset m) of
+    Nothing -> False
+    Just _ -> True
 
 -- | Returns the count of an element in the multiset, 0 if not present.
 count :: Ord a => a -> MultiSet a -> Int
+count x m = case lookupS x (_getMultiset m) of
+    Nothing -> 0
+    Just y -> y
 
 -- | Insert one occurrence of an element into the multiset.
 insert :: Ord a => a -> MultiSet a -> MultiSet a
+insert x (MultiSet s) = case lookupS x s of
+  Nothing -> MultiSet $ Set.insert (Arg x 1) s
+  Just n -> MultiSet $ Set.insert (Arg x (n + 1)) (Set.delete (Arg x n) s)
 
 -- | Remove one occurrence of an element from the multiset.
 remove :: Ord a => a -> MultiSet a -> MultiSet a
+remove x (MultiSet s) = case lookupS x s of
+    Nothing -> MultiSet s
+    Just 1 -> MultiSet $ Set.delete (Arg x 1) s
+    Just n -> MultiSet $ Set.insert (Arg x (n - 1)) (Set.delete (Arg x n) s)
 
 -- | Convert a list into a multiset.
 fromList :: Ord a => [a] -> MultiSet a
+fromList = foldr insert empty
 
 -- | Convert a multiset into a list, including duplicates.
 toList :: Ord a => MultiSet a -> [a]
+toList m = Set.foldr insertVal [] (_getMultiset m)
+    where
+        insertVal :: Arg a Int -> [a] -> [a]
+        insertVal (Arg a b) l = replicate b a ++ l
 
+instance Ord a => Eq (MultiSet a) where
+  (==) :: Ord a => MultiSet a -> MultiSet a -> Bool
+  m1 == m2 = toList m1 == toList m2
 
-instance Eq a => Eq (MultiSet a)
-instance Show a => Show (MultiSet a)
-instance Ord a => Semigroup (MultiSet a)
-instance Ord a => Monoid (MultiSet a)
+-- todo: need spaces between ","?
+instance Show a => Show (MultiSet a) where
+  show :: Show a => MultiSet a -> String
+  show m =
+    let elems = Set.foldr insertVal [] (_getMultiset m)
+        insertVal (Arg a b) l = replicate b (show a) ++ l
+    in "{" ++ intercalate "," elems ++ "}"
 
+instance Ord a => Semigroup (MultiSet a) where
+     (<>) :: Ord a => MultiSet a -> MultiSet a -> MultiSet a
+     MultiSet s1 <> MultiSet s2 = fromList (toList (MultiSet s1) ++ toList (MultiSet s2))
+
+instance Ord a => Monoid (MultiSet a) where
+    mempty :: Ord a => MultiSet a
+    mempty = empty
 
 ---------------- Ex. 5 --------------------- 
 
-instance Foldable (Multiset a) where
-
+-- instance Foldable (MultiSet a) where
+    -- changed sig
+instance Foldable MultiSet where
+    foldr :: (a -> b -> b) -> b -> MultiSet a -> b
+    foldr f b m = foldr f b (toList m)
 
 -- | /O(n)/. Fold over the elements of a multiset with their occurrences.
 foldOccur :: (a -> Int -> b -> b) -> b -> MultiSet a -> b
+foldOccur f s (MultiSet m) = Set.foldr (\(Arg a b) x -> f a b x) s m
+
+
+
 
