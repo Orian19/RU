14d07699f47242eeee1d26c69b8a157a29c5390b
Orian19 <orian19@gmail.com>
2025-06-10 19:04:18 +0300

    [3][B][FunctionalProgramming][5] finish section 3 + section 4

--- ThirdYear/SemesterB/FunctionalProgramming/HW5/HW5.hs
+++ ThirdYear/SemesterB/FunctionalProgramming/HW5/HW5.hs
@@ -18,21 +18,21 @@ import Data.Semigroup (Arg (..), Max (..), Min (..))
 import Data.Set (Set)
 import Data.Set qualified as S
 import MultiSet
-import Prelude (Bool (..), Char, Either (..), Enum (..), Eq (..), Foldable (foldMap, foldl, foldr), Fractional, Functor (fmap), Int, Maybe (..), Monoid (..), Num (..), Ord (..), Ordering (..), Semigroup (..), Show (..), String, all, and, any, concat, concatMap, const, curry, drop, dropWhile, error, filter, flip, fst, id, init, map, not, or, replicate, reverse, snd, take, takeWhile, uncurry, undefined, zip, zipWith, (!!), ($), (&&), (++), (.), (||))
+import Prelude (Bool (..), Char, Either (..), Enum (..), Eq (..), Foldable (foldMap, foldl, foldr), Fractional, Functor (fmap), Int, Maybe (..), Monoid (..), Num (..), Ord (..), Ordering (..), Semigroup (..), Show (..), String, all, and, any, concat, concatMap, const, curry, drop, dropWhile, error, filter, flip, fst, id, init, map, not, or, replicate, reverse, snd, take, takeWhile, uncurry, undefined, zip, zipWith, (!!), ($), (&&), (++), (.), (||), (/), fromIntegral, (<$>))
 
 -- Section 1: Foldable functions
 fold :: (Foldable t, Monoid a) => t a -> a
 fold = foldMap id
 
 toList :: Foldable t => t a -> [a]
-toList = foldmap (:[])
+toList = foldMap (:[])
 
 elem :: (Foldable t, Eq a) => a -> t a -> Bool
 elem = any . (==)
 
 find :: (Foldable t, Eq a) => (a -> Bool) -> t a -> Maybe a
 -- uses toList
-find p ta = case filter p (toList ta) of
+find p ta = case filter p (HW5.toList ta) of
   [] -> Nothing
   (x: _) -> Just x
 
@@ -74,7 +74,12 @@ data Fold a b c = Fold (b -> a -> b) b (b -> c)
 
 -- Instances + helpers
 
-instance Functor (Fold a b c)
+instance Functor (Fold a b) where
+  fmap :: (c -> d) -> Fold a b c -> Fold a b d
+  fmap f (Fold a b c) = Fold a b (f . c) 
+
+combine :: Fold a b c -> Fold a b' c' -> Fold a (b, b') (c, c')
+combineWith :: (c -> c' -> d) -> Fold a b c -> Fold a b' c' -> Fold a (b, b') d
 
 -- From the lectures
 class Functor f => Apply f where
@@ -84,44 +89,99 @@ class Functor f => Apply f where
   (<*>) :: f (a -> b) -> f a -> f b
   (<*>) = liftA2 id
 
-instance Apply (Fold a b)
+instance Apply (Fold a b) where
+  liftA2 :: (c -> d -> e) -> Fold a b c -> Fold a b d -> Fold a b e
+  liftA2 f (Fold a b c) (Fold a' b' d) =
+    Fold
+      (\(x, y) z ->
+        let x' = a x z
+            y' = a' y z
+        in (x', y'))
+      (b, b')
+      (\(x, y) -> 
+        let cx = c x
+            dy = d y
+        in f cx dy)
 
 -- Execute a fold operation as a left fold.
 runFold :: Foldable t => Fold a b c -> t a -> c
+runFold (Fold a b c) ta = c (foldl' a b ta)
 -- "F" companions to normal fold utils
 
 -- Will only fold over elements that satisfy a predicate
 filterF :: (a -> Bool) -> Fold a b c -> Fold a b c
+filterF p (Fold a b c) = Fold (\x y -> if p y then a x y else x) b c
+
 -- Applies a function to each elements before applying the step function
 mapF :: (a -> a) -> Fold a b c -> Fold a b c -- Not to be confused with fmap!
+mapF f (Fold a b c) = Fold (\x y -> a x (f y)) b c
+
+-- nullF :: Fold a Bool Bool
 nullF :: Fold a b Bool
-findF :: (a -> Bool) -> Fold a b (Maybe a)
-topKF :: Ord a -> Fold a b [a]
+nullF = Fold (\_ _ -> False) True id
+
+findF :: (a -> Bool) -> Fold a (Maybe a) (Maybe a)
+findF p = Fold (\x y -> if p y then Just y else x) Nothing id
+
+topKF :: Ord a => Int -> Fold a b [a]
+-- todo: fix with int
+topKF k = Fold (:) [] sort
+
 -- Mathematical folds
-sumF :: Num a => Fold a a
-productF :: Num a => Fold a a
-lengthF :: Fold a Int
-averageF :: Fractional a => Fold a a a
-varianceF :: Fractional a => Fold a a a
+sumF :: Num a => Fold a a a
+sumF = Fold (+) 0 id
+
+productF :: Num a => Fold a a a
+productF = Fold (*) 1 id
+
+lengthF :: Fold a Int Int
+lengthF = Fold (\_ y -> y + 1) 0 id
+
+-- averageF :: Fractional a => Fold a (a, b) a
+averageF :: Fractional a => Fold a (a, Int) a
+averageF = Fold step (0, 0) extract
+  where
+    step (s, c) x = (s + x, c + 1)
+    extract (s, c) = if c == 0 then 0 else s / fromIntegral c
+
 -- Section 3: Functor functions
 
 fmapToFst :: Functor f => (a -> b) -> f a -> f (b, a)
+fmapToFst f fa = fmap (\x -> (f x, x)) fa
+
 fmapToSnd :: Functor f => (a -> b) -> f a -> f (a, b)
+fmapToSnd f fa = fmap (\x -> (x, f x)) fa
+
 strengthenL :: Functor f => b -> f a -> f (b, a)
+strengthenL b fa = fmap (\x -> (b, x)) fa
+
 strengthenR :: Functor f => b -> f a -> f (a, b)
+strengthenR b fa = fmap (\x -> (x, b)) fa
+
 unzip :: Functor f => f (a, b) -> (f a, f b)
+unzip fab = (fmap fst fab, fmap snd fab)
+
 coUnzip :: Functor f => Either (f a) (f b) -> f (Either a b)
+coUnzip = \case
+  Left fa -> fmap Left fa
+  Right fb -> fmap Right fb
 
 -- Section 4: MultiSet Foldable instances
 
 newtype FoldOccur a = FoldOccur {getFoldOccur :: MultiSet a}
-instance Foldable FoldOccur
+instance Foldable FoldOccur where
+  foldr :: (a -> b -> b) -> b -> FoldOccur a -> b
+  foldr f b (FoldOccur a) = MultiSet.foldOccur (\x _ z -> f x z) b a
 
 newtype MinToMax a = MinToMax {getMinToMax :: MultiSet a}
-instance Ord a => Foldable MinToMax
+instance Foldable MinToMax where
+  foldr :: (a -> b -> b) -> b -> MinToMax a -> b
+  foldr f b (MinToMax a) = MultiSet.foldr f b (sort (MultiSet.toList a))
 
-newtype MaxToMin a = MinToMax {getMinToMax :: MultiSet a}
-instance Ord a => Foldable MaxToMin
+newtype MaxToMin a = MaxToMin {getMaxToMin :: MultiSet a}
+instance Foldable MaxToMin where
+  foldr :: (a -> b -> b) -> b -> MinToMax a -> b
+  foldr f b (MinToMax a) = MultiSet.foldr f b (reverse (sort (MultiSet.toList a)))
 
 -- Bonus section
 
@@ -129,3 +189,14 @@ newtype ZipList a = ZipList {getZipList :: [a]} deriving (Show, Eq)
 
 instance Semigroup a => Semigroup (ZipList a)
 instance Monoid a => Monoid (ZipList a)
+
+-- Bonus (5 pt.): implement the varianceF Fold. This is slightly harder than average - the formula is
+-- E[X^2] - E[x]^2, so you need to keep track of more than two quantities in your tuple. Use `combineWith` as needed.
+varianceF :: Fractional a => Fold a b a
+varianceF = Fold
+  (\(s, sumSq, c) x -> (s + x, sumSq + x * x, c + 1)) 
+  (0, 0, 0)
+  (\(s, sumSq, c) -> 
+    if c == 0
+      then 0
+      else (sumSq / fromIntegral c) - (s / fromIntegral c) ^ 2)
